85881799bb30cad2a2c39d183903a5ff157194b2
vijaynailwal1992 <vijaynailwal92@gmail.com>
2019-03-04 18:37:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewclicks/AppUtility.java
@@ -0,0 +1,111 @@
+package com.example.recyclerviewclicks;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AppUtility {
+    private String [] names;
+    private String[] lastnames;
+    private String [] genders;
+    private String [] nationalities;
+
+    private String [] uniqueNationalitiesArray;
+
+    private static final String EMPTY_STRING = "";
+
+    private Context context;
+
+    private List<Person> people;
+
+    private static AppUtility appUtility;
+
+    private AppUtility(Context context){
+        this.context=context;
+        names=context.getResources().getStringArray(R.array.names);
+        lastnames = context.getResources().getStringArray(R.array.lastnames);
+        genders = context.getResources().getStringArray(R.array.gender);
+        nationalities=context.getResources().getStringArray(R.array.nationality);
+        people=new ArrayList<Person>();
+        for(int i=0;i<names.length;i++){
+            Person person=new Person(names[i],lastnames[i],
+                                     genders[i].equalsIgnoreCase("male")? Person.GENDER.MALE: Person.GENDER.FEMALE,
+                                     nationalities[i]);
+            people.add(person);
+        }
+
+        setUniqueNationalities();
+
+    }
+
+    public static AppUtility getAppUtility(Context context){
+        if(appUtility==null){
+            appUtility=new AppUtility(context);
+        }
+        return appUtility;
+    }
+
+    public List<Person> getPeople(){
+        return this.people;
+    }
+
+    public String[] getNames() {
+        return names;
+    }
+
+    public String[] getLastnames() {
+        return lastnames;
+    }
+
+    public String[] getGenders() {
+        return genders;
+    }
+
+    public String [] getNationalities(){
+        return nationalities;
+    }
+
+    public static boolean isStringEmpty(String string){
+        boolean isStringEmpty=false;
+        if (string==null){
+            isStringEmpty=true;
+        }else{
+            if(string.length()==0 || string.equals(EMPTY_STRING)){
+                isStringEmpty=true;
+            }
+        }
+        return isStringEmpty;
+    }
+
+    private void setUniqueNationalities(){
+        List<String> uniqueNationalities = new ArrayList<String>();
+        for(String nationality: getNationalities()){
+            if(!uniqueNationalities.contains(nationality)){
+                uniqueNationalities.add(nationality);
+            }
+        }
+        uniqueNationalitiesArray = uniqueNationalities.toArray(new String[uniqueNationalities.size()]);
+    }
+
+    public String[] getUniqueNationalitiesArray(){
+        return this.uniqueNationalitiesArray;
+    }
+
+
+
+    public int getNationalityForSelectedIndex(String nationality){
+        int nationalityIndex=0;
+        String [] nationalities = this.uniqueNationalitiesArray;
+        do{
+            if(nationalities[nationalityIndex].equals(nationality)){
+                break;
+            }
+            nationalityIndex++;
+        }while (nationalityIndex<nationalities.length);
+
+        return nationalityIndex;
+    }
+
+}
---
+++ app/src/main/java/com/example/recyclerviewclicks/MainActivity.java
@@ -0,0 +1,181 @@
+package com.example.recyclerviewclicks;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ListAdapterWithRecycleView.PersonModifier {
+
+    private RecyclerView recyclerView;
+    AppUtility appUtility;
+    ListAdapterWithRecycleView listAdapterWithRecycleView;
+    private EditText editTextFirstName, editTextLastName;
+    private Spinner spinnerNationality;
+
+    private RadioGroup radioGroup;
+    private Button buttonAdd;
+    List<Person> people;
+    int modificationIndex = -1;
+    String firstName, lastName, nationality;
+    Person.GENDER gender;
+    LinearLayoutManager linearLayoutManager;
+    /*GridLayoutManager gridLayoutManager;
+    StaggeredGridLayoutManager staggeredGridLayoutManager;
+*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        appUtility = AppUtility.getAppUtility(getApplicationContext());
+        initPersonInputForm();
+        recyclerView = findViewById(R.id.recycleListView);
+
+        people = appUtility.getPeople();
+
+        listAdapterWithRecycleView = new ListAdapterWithRecycleView(this, people);
+        listAdapterWithRecycleView.setPersonModifier(this);
+
+        linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        /*gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        staggeredGridLayoutManager = new StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.HORIZONTAL);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);*/
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(listAdapterWithRecycleView);
+    }
+
+    private void initPersonInputForm() {
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        radioGroup = findViewById(R.id.radioGroupGender);
+        spinnerNationality = findViewById(R.id.spinnerNationality);
+        spinnerNationality.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, appUtility.getUniqueNationalitiesArray()));
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonAdd.setTag("Add");
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+                switch (i) {
+                    case R.id.radioButtonMale:
+                        gender = Person.GENDER.MALE;
+                        break;
+                    case R.id.radioButtonFemale:
+                        gender = Person.GENDER.FEMALE;
+                        break;
+                    default:
+                        gender = null;
+                }
+            }
+        });
+
+        spinnerNationality.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                nationality = getResources().getStringArray(R.array.nationalities)[i];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstName = editTextFirstName.getText().toString();
+                lastName = editTextLastName.getText().toString();
+                Person person = null;
+
+                if (isInputDataValid()) {
+                    person = new Person(firstName, lastName, gender, nationality);
+                } else {
+                    Toast.makeText(MainActivity.this, "Input Invalid", Toast.LENGTH_LONG).show();
+                }
+
+                String behaviour = (String) buttonAdd.getTag();
+                if (behaviour.equalsIgnoreCase("Add")) {
+                    if (person != null) {
+                        people.add(person);
+                        listAdapterWithRecycleView.notifyDataSetChanged();
+                        recyclerView.scrollToPosition(people.size() - 1);
+                        clearInputForm();
+                    }
+                } else if (behaviour.equalsIgnoreCase("modify")) {
+                    if (person != null) {
+                        try {
+                            people.get(modificationIndex).setName(person.getName());
+                            people.get(modificationIndex).setLastName(person.getLastName());
+                            people.get(modificationIndex).setGender(person.getGender());
+                            people.get(modificationIndex).setNationality((String) spinnerNationality.getSelectedItem());
+
+                            listAdapterWithRecycleView.notifyItemChanged(modificationIndex);
+                            clearInputForm();
+                            buttonAdd.setTag("Add");
+                            buttonAdd.setText("Add");
+                        } catch (IndexOutOfBoundsException exception) {
+                            Toast.makeText(MainActivity.this, "Can't modify, item moved", Toast.LENGTH_LONG).show();
+                            listAdapterWithRecycleView.notifyDataSetChanged();
+                            clearInputForm();
+                            buttonAdd.setTag("Add");
+                            buttonAdd.setText("Add");
+                        }
+                    }
+                }
+
+            }
+        });
+    }
+
+    private boolean isInputDataValid() {
+        if (AppUtility.isStringEmpty(firstName) || AppUtility.isStringEmpty(lastName) || AppUtility.isStringEmpty(nationality) || gender == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void clearInputForm() {
+        editTextFirstName.setText("");
+        editTextLastName.setText("");
+        radioGroup.clearCheck();
+        spinnerNationality.setSelection(0);
+    }
+
+    @Override
+    public void onPersonSelected(int position) {
+        modificationIndex = position;
+        Person person = people.get(position);
+        buttonAdd.setTag("Modify");
+        buttonAdd.setText("Modify");
+        editTextFirstName.setText(person.getName());
+        editTextLastName.setText(person.getLastName());
+        if (person.getGender() == Person.GENDER.MALE) {
+            ((RadioButton) findViewById(R.id.radioButtonMale)).performClick();
+        } else if (person.getGender() == Person.GENDER.FEMALE) {
+            ((RadioButton) findViewById(R.id.radioButtonFemale)).performClick();
+        }
+        spinnerNationality.setSelection(appUtility.getNationalityForSelectedIndex(person.getNationality()));
+    }
+
+    @Override
+    public void onPersonDeleted(int position) {
+        buttonAdd.setTag("Add");
+        buttonAdd.setText("Add");
+        clearInputForm();
+    }
+}
